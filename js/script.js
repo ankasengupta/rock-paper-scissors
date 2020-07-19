1dfa67d73e231ebd1ded9200818e803a00e24936
Anoushka Sengupta <51234849+ankasengupta@users.noreply.github.com>
2020-07-19 20:26:04 +0530

    Update script.js
    added more comments to increase readability

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const paperDiv = document.getElementById("paper");
 const scissorDiv = document.getElementById("scissor");
 
 /**
+ * To randomly decide the option that the computer chooses
  * @returns array element at randomly generated point between 0-3
  */
 function getChoiceComp() {
@@ -17,22 +18,41 @@ function getChoiceComp() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
+/**
+ * To update the scoreboard and message displayed when the user wins
+ * @param {*} choiceUser the option the user chose
+ * @param {*} choiceComp the option the computer chose
+ */
 function win(choiceUser, choiceComp) {
     scoreUser++;
     scoreUserSpan.innerHTML = scoreUser;
     resultDiv.innerHTML = `${choiceUser.charAt(0).toUpperCase()}${choiceUser.slice(1)} beats ${choiceComp}. You win!`;
 }
 
+/**
+ * To update the scoreboard and message displayed when the user loses
+ * @param {*} choiceUser the option the computer chose
+ * @param {*} choiceComp the option the user chose
+ */
 function lose(choiceUser, choiceComp) {
     scoreComp++;
     scoreCompSpan.innerHTML = scoreComp;
     resultDiv.innerHTML = `${choiceComp.charAt(0).toUpperCase()}${choiceComp.slice(1)} beats ${choiceUser}. You lose.`;
 }
 
+/**
+ * To update the message displayed when there is a draw
+ * @param {*} choiceUser the option the user chose
+ * @param {*} choiceComp the option the computer chose
+ */
 function draw(choiceUser, choiceComp) {
     resultDiv.innerHTML = `${choiceComp.charAt(0).toUpperCase()}${choiceComp.slice(1)} equals ${choiceUser}. It's a draw.`;
 }
 
+/**
+ * To call functions depending on the case - win, lose or draw
+ * @param {*} choiceUser the option the user chose
+ */
 function game(choiceUser) {
     const choiceComp = getChoiceComp();
     switch(choiceUser.charAt(0) +  choiceComp.charAt(0)) {
